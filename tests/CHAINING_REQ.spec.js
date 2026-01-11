0b8720504cd306e7f49fe57791b76e90b72933e4
truonglb24 <truongleba.hvtc@gmail.com>
2026-01-11 17:48:05 +0700

    chaining req

---
+++ tests/CHAINING_REQ.spec.js
@@ -0,0 +1,52 @@
+/*
+ - Create order -> Read order details -> Update order -> Delete order || CRUD
+*/
+const { test, expect } = require("@playwright/test");
+
+
+test('should be able to perform CRUD on post type', async ({request})=>{
+    
+    // Consstruct data
+    const baseUrl = "https://jsonplaceholder.typicode.com";
+    const postContent = {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    };
+    const putContent = {
+        id: 1,
+        title: 'truonglb',
+        body: 'this is body',
+        userId: 1,
+    };
+    const headers = {
+        'Content-type': 'application/json; charset=UTF-8'
+    };
+    const postOptions = {
+        headers:headers,
+        data: postContent
+    };
+    const putOptions = {
+        headers:headers,
+        data: putContent
+    };
+    // Create a post
+    const postRes = await request.post(baseUrl + '/posts', postOptions);
+    const resBody = await postRes.json();
+    let postId = resBody.id; // 101
+    postId = Number(postId) - 1; // 100
+    // TODO: Verification
+
+    // Reuse the post ID to read the details
+    const getRes = await request.get(`${baseUrl}/posts/${postId}`);
+    // TODO: Verification
+
+    // Update the post
+    const putRes = await request.put(`${baseUrl}/posts/${postId}`, putOptions);
+    // TODO: Verification
+
+    // Delete the post
+    const deleteRes = await request.delete(`${baseUrl}/posts/${postId}`);
+    // TODO: Verification
+
+})
